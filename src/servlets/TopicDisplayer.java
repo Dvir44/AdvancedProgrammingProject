b34dc6aa8ef629a53984ebd9fc80b66aad1f4008
hilla2702 <hillali2702@gmail.com>
2024-07-30 18:33:10 +0300

    update TopicDisplayer class

--- src/servlets/TopicDisplayer.java
+++ src/servlets/TopicDisplayer.java
@@ -2,18 +2,41 @@ package servlets;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
 
+import graph.Message;
+import graph.Topic;
+import graph.TopicManagerSingleton;
 import server.RequestParser.RequestInfo;
 
 public class TopicDisplayer implements Servlet {
 
 	@Override
 	public void handle(RequestInfo ri, OutputStream toClient) throws IOException {
-		
+		if (!"GET".equals(ri.getHttpCommand())) {
+			toClient.write("HTTP/1.1 405 Method Not Allowed\r\n\r\n".getBytes(StandardCharsets.UTF_8));
+			toClient.flush();
+			return;
+		}
+
+		Map<String, String> parameters = ri.getParameters();
+		String topicName = parameters.get("topic");
+		String messageContent = parameters.get("message");
+
+		if (topicName != null && messageContent != null) {
+			Topic topic = TopicManagerSingleton.get().getTopic(topicName);
+			Message message = new Message(messageContent);
+			topic.publish(message);
+		}
+
+		String response = "HTTP/1.1 200 OK\r\n\r\n";
+		toClient.write(response.getBytes(StandardCharsets.UTF_8));
+		toClient.flush();
 	}
-	
+
 	@Override
 	public void close() throws IOException {
-		
+		TopicManagerSingleton.get().clear();
 	}
 }
