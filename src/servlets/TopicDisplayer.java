8eb349321863e479db02eebf0c9d206daa766514
hilla2702 <hillali2702@gmail.com>
2024-07-30 18:36:40 +0300

    commends for class TopicDisplayer

--- src/servlets/TopicDisplayer.java
+++ src/servlets/TopicDisplayer.java
@@ -14,23 +14,31 @@ public class TopicDisplayer implements Servlet {
 
 	@Override
 	public void handle(RequestInfo ri, OutputStream toClient) throws IOException {
+		// Check if the command is GET
 		if (!"GET".equals(ri.getHttpCommand())) {
+			// Send a response with status code 405 (Method Not Allowed) if the command is
+			// not GET
 			toClient.write("HTTP/1.1 405 Method Not Allowed\r\n\r\n".getBytes(StandardCharsets.UTF_8));
 			toClient.flush();
 			return;
 		}
 
+		// Extract parameters from the RequestInfo
 		Map<String, String> parameters = ri.getParameters();
 		String topicName = parameters.get("topic");
 		String messageContent = parameters.get("message");
 
+		// If both "topic" and "message" parameters are found
 		if (topicName != null && messageContent != null) {
+			// Get the topic and publish the message to it
 			Topic topic = TopicManagerSingleton.get().getTopic(topicName);
 			Message message = new Message(messageContent);
 			topic.publish(message);
 		}
 
+		// Create a response with status code 200 (OK)
 		String response = "HTTP/1.1 200 OK\r\n\r\n";
+		// Send the response to the client
 		toClient.write(response.getBytes(StandardCharsets.UTF_8));
 		toClient.flush();
 	}
