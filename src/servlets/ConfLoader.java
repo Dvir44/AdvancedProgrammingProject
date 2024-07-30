f3407dbcb1f79bf5cfcaa61511a3c69fd02fd02f
Dvir44 <dvi4408@gmail.com>
2024-07-30 13:12:17 +0300

    ex6 template
    the template with all the necessary files (without the html one)

---
+++ src/servlets/ConfLoader.java
@@ -0,0 +1,18 @@
+package servlets;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+import server.RequestParser.RequestInfo;
+
+public class ConfLoader implements Servlet {
+	@Override
+	public void handle(RequestInfo ri, OutputStream toClient) throws IOException {
+		
+	}
+	
+	@Override
+	public void close() throws IOException {
+		
+	}
+}
---
+++ src/servlets/HtmlLoader.java
@@ -0,0 +1,19 @@
+package servlets;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+import server.RequestParser.RequestInfo;
+
+public class HtmlLoader implements Servlet {
+	
+	@Override
+	public void handle(RequestInfo ri, OutputStream toClient) throws IOException {
+		
+	}
+	
+	@Override
+	public void close() throws IOException {
+		
+	}
+}
