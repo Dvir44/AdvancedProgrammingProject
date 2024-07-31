e9a4cdd43e87ace356ee6d237014c6b0a9886c6d
Dvir44 <dvi4408@gmail.com>
2024-08-01 01:04:49 +0300

    working generic class
    the working one from Hilla cause mine did problems with reading the files

--- src/configs/GenericConfig.java
+++ src/configs/GenericConfig.java
@@ -1,93 +1,96 @@
 package configs;
 
-import java.util.Arrays;
-import java.util.List;
 import java.lang.reflect.Constructor;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.util.List;
 
-
-import graph.Message;
+import graph.Agent;
 import graph.ParallelAgent;
 import graph.TopicManagerSingleton;
-import graph.Agent;
 
 public class GenericConfig implements Config {
-	
-	private List<Agent> agents;
-	private String file_name;
-	private final int capacity = 30; // set a capacity for the number of agents that can be in the parallel class
-	
-	public GenericConfig() {
-		this.agents = new ArrayList<Agent>(); // set an array for the agents
-		this.file_name = "";
-	}
-	
-	public void setConfFile(String file_name) {
-		this.file_name = file_name;
-	}
-	
-	@Override
-	public void create() {
-		String[] subs;
-		String[] pubs;
-		List<String> file_lines = new ArrayList<String>();
-		try (BufferedReader reader = new BufferedReader(new FileReader(this.file_name))){
-			String line;
-			while ((line = reader.readLine()) != null) {
-				file_lines.add(line.trim());
-			}
-			
-			// checking that the length is valid
-			if (file_lines.size() % 3 != 0) {
-				System.err.println("error: the configuration file must have a number of lines that is a multiple of 3");
-				return;
-			}
-			System.out.println(file_lines);
-			System.out.println(file_lines.get(0));
-			
-			// read every 3 lines now
-			for (int i=0; i<file_lines.size(); i+=3) {
-				// subs and pubs
-				subs = file_lines.get(i + 1).split(",");
-				pubs = file_lines.get(i+2).split(",");
-				System.out.println(subs);
-				System.out.println(i);
-				
-				System.out.println(pubs);
-				System.out.println(i);
-				// building the agent
-				Class<?> class1 = Class.forName(file_lines.get(i));
-                Constructor<?> constructor = class1.getConstructor(subs.getClass(), pubs.getClass());
-                Agent agent = (Agent) constructor.newInstance(subs, pubs);
+    private List<Agent> agents; // List to store agents
+    private String confFile; // Path to the configuration file
 
-                Agent parallelAgent = new ParallelAgent(agent, capacity);
-                agents.add(parallelAgent);
-			}
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	@Override
-	public String getName() {
-		return "Generic Config";
-	}
-	
-	@Override
-	public int getVersion() {
-		return 1;
-	}
-	
-	@Override
-	public void close() { // calls the close method of each one of the agents
-		for (Agent agent : this.agents) {
-			agent.close();
-		}
-	}
+    public GenericConfig() {
+        agents = new ArrayList<>(); // Initialize agents list
+        this.confFile = ""; // Initialize configuration file path
+    }
+
+    // Set the configuration file path and close any existing agents
+    public void setConfFile(String file) {
+        if (!this.confFile.equals(file)) {
+            this.confFile = file;
+            this.close(); // Close existing agents if the file path changes
+        }
+    }
+
+    @Override
+    public void create() {
+        List<String> file_lines; // Lines read from the configuration file
+        int capacity = 50; // Capacity for ParallelAgent
+        try {
+            file_lines = Files.readAllLines(Paths.get(this.confFile)); // Read all lines from the file
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("error can not read this file " + this.confFile + " in config " + this.getName());
+            return;
+        }
+
+        // Check if the number of lines is divisible by 3
+        if (file_lines.size() % 3 != 0) {
+            System.out.println("Incorrect input of the file:" + this.confFile);
+            return;
+        }
+
+        // Process lines in groups of three
+        for (int i = 0; i < file_lines.size(); i = i + 3) {
+            String className = file_lines.get(i); // Class name of the agent
+            String[] subs = file_lines.get(i + 1).split(","); // Subscription topics
+            String[] pubs = file_lines.get(i + 2).split(","); // Publication topics
+
+            try {
+                // Load the class and create an instance using reflection
+                Class<?> agentClass = Class.forName(className);
+                Constructor<?> constructor = agentClass.getConstructor(String[].class, String[].class);
+                Agent agent = (Agent) constructor.newInstance((Object) subs, (Object) pubs);
+                Agent parallelAgent = new ParallelAgent(agent, capacity); // Wrap in ParallelAgent
+
+                // Add agent if not already present
+                if (agents.stream().noneMatch(parallelAgent::equals)) {
+                    agents.add(parallelAgent);
+                } else {
+                    parallelAgent.close(); // Close if already present
+                }
+            } catch (ClassNotFoundException e) {
+                System.out.println("No class matches line " + (i + 1) + ": " + className + " in file " + this.confFile);
+            } catch (NoSuchMethodException e) {
+                System.out
+                        .println("No suitable constructor found for class " + className + " in file " + this.confFile);
+            } catch (Exception e) {
+                System.out.println("Failed to instantiate class " + className + " in file " + this.confFile);
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public String getName() {
+        return "GenericConfig"; // Return the name of the configuration
+    }
+
+    @Override
+    public int getVersion() {
+        return 1; // Return the version of the configuration
+    }
+
+    @Override
+    public void close() {
+        for (Agent a : this.agents) { // Close all agents in the list
+            a.close();
+        }
+        TopicManagerSingleton.get().clear(); // Clear all topics in the TopicManager
+    }
 }
